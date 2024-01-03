cde1f7f3ad9e1a9c632fea872a44bd131bb3e74f
joanclarke <jcjuju2014@gmail.com>
2024-01-03 17:21:00 -0500

    create models and design schemas

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
+const userRoute = require('./routes/user')
 
 dotenv.config()
 
@@ -11,6 +12,13 @@ mongoose
   .catch((err) => {
     console.log(err)
   })
+
+// app.use('/api/test', () => {
+//   console.log('test is successful')
+// })
+
+app.use(express.json())
+app.use('/api/users', userRoute)
 
 app.listen(process.env.PORT || 5000, () => {
   console.log('Backend Server is running!')
