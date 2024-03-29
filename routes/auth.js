b4edcc3db2b47e6ba788d6e52693796e0b64f9d6
joanclarke <jcjuju2014@gmail.com>
2024-03-28 21:13:40 -0400

    authenticate using crypto-js, create users with encrypted passwords in database

--- routes/auth.js
+++ routes/auth.js
@@ -1,13 +1,20 @@
 const router = require('express').Router()
 const User = require('../models/User')
+// const cryptojs = require('crypto.js')
+// const CryptoJS = require('crypto.js')
+const CryptoJS = require('crypto-js')
 
 //REGISTER
 router.post('/register', async (req, res) => {
   const newUser = new User({
     username: req.body.username,
     email: req.body.email,
-    password: req.body.password,
+    password: CryptoJS.AES.encrypt(
+      req.body.password,
+      process.env.PASS_SEC
+    ).toString(),
   })
+
   try {
     const savedUser = await newUser.save()
     res.status(201).json(savedUser)
