d7a33a0b529d5eadd23ad382e2dea4d149e11d0b
joanclarke <jcjuju2014@gmail.com>
2024-04-19 20:59:05 -0400

    securing with accessToken using  jsonwebtoken

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,7 @@
 const router = require('express').Router()
 const User = require('../models/User')
-// const cryptojs = require('crypto.js')
-// const CryptoJS = require('crypto.js')
 const CryptoJS = require('crypto-js')
+const jwt = require('jsonwebtoken')
 
 //REGISTER
 router.post('/register', async (req, res) => {
@@ -23,24 +22,38 @@ router.post('/register', async (req, res) => {
   }
 })
 
-// LOGIN
+//LOGIN
 
 router.post('/login', async (req, res) => {
   try {
-    const user = await User.findOne({ username: req.body.username })
-    !user && res.status(401).json('Incorrect credentials!')
+    const user = await User.findOne({
+      userName: req.body.user_name,
+    })
+
+    !user && res.status(401).json('Wrong User Name')
 
     const hashedPassword = CryptoJS.AES.decrypt(
       user.password,
       process.env.PASS_SEC
     )
-    const OriginalPassword = hashedPassword.toString(CryptoJS.enc.utf8)
-    OriginalPassword !== req.body.password &&
-      res.status(401).json('Incorrect credentials!')
+
+    const originalPassword = hashedPassword.toString(CryptoJS.enc.Utf8)
+
+    const inputPassword = req.body.password
+
+    originalPassword != inputPassword && res.status(401).json('Wrong Password')
+
+    const accessToken = jwt.sign(
+      {
+        id: user._id,
+        isAdmin: user.isAdmin,
+      },
+      process.env.JWT_SEC,
+      { expiresIn: '3d' }
+    )
 
     const { password, ...others } = user._doc
-
-    res.status(200).json(others)
+    res.status(200).json({ ...others, accessToken })
   } catch (err) {
     res.status(500).json(err)
   }
@@ -48,4 +61,4 @@ router.post('/login', async (req, res) => {
 
 module.exports = router
 
-// Next : 50:30 on video
+// Next : 54:16 on video
